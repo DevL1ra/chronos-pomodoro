dc9cdef2ae5b56676321849e08a30958abf1d2a8
Renan Figueiredo <renan.figueiredo@econverse.com.br>
2025-11-20 17:11:46 -0300

    Merge: Integrando atualizações do DevL1ra/main

--- eslint.config.js
+++ eslint.config.js
@@ -1,34 +1,18 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
-<<<<<<< HEAD
+import js from '@eslint/js';
+import globals from 'globals';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import tseslint from 'typescript-eslint';
 
 export default tseslint.config(
   { ignores: ['dist'] },
   {
     extends: [js.configs.recommended, ...tseslint.configs.recommended],
     files: ['**/*.{ts,tsx}'],
-=======
-import { globalIgnores } from 'eslint/config'
-
-export default tseslint.config([
-  globalIgnores(['dist']),
-  {
-    files: ['**/*.{ts,tsx}'],
-    extends: [
-      js.configs.recommended,
-      tseslint.configs.recommended,
-      reactHooks.configs['recommended-latest'],
-      reactRefresh.configs.vite,
-    ],
->>>>>>> 01d1063b1e23d1ffea5e89c69fb089f9f08f7b81
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
     },
-<<<<<<< HEAD
     plugins: {
       'react-hooks': reactHooks,
       'react-refresh': reactRefresh,
@@ -41,8 +25,4 @@ export default tseslint.config([
       ],
     },
   },
-)
-=======
-  },
-])
->>>>>>> 01d1063b1e23d1ffea5e89c69fb089f9f08f7b81
+);
